f7ace9e1cac1d7e33dcc1095dcc0b115d374fea8
priyamathur22 <priyamathur2027@gmail.com>
2026-08-12 10:09:06 +0530

    Learn about variables

---
+++ code.py
@@ -0,0 +1,57 @@
+# print("Hii!", " I am Priya Mathur")
+
+# variables 
+"""
+name = 'Priya Mathur'
+age = 21
+#print(name, age)
+name = 'Mathur '
+age = 22
+cgpa = 7.7
+isStudent = True
+print(name, age, cgpa)
+
+print(type(name))
+print(type(cgpa))
+print(type(isStudent))
+"""
+
+# input function
+"""
+name = input("Enter your name: ")
+print("Hello " + name) #concatenation
+print("Hello", name) #comma separated
+"""
+#exercise 1
+"""
+first_name = "Tony"
+last_name  = "Stark"
+age = 53
+height = "1.85m"
+details = print("My name is " + first_name + " " + last_name + ". I am " + str(age) + " years old and my height is " + height + ".")
+
+secret = input("Enter your secret: ")
+print("Tony stark is secretly a " + secret + ".")
+"""
+
+#type conversion
+"""
+age = (input("Enter your age: ")) # string input 
+print(type(age))
+age = int(age) # type conversion to integer
+print(type(age))
+"""
+
+# sum program 
+"""
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+sum = a + b 
+print("sum:" , sum ) 
+"""
+
+
+
+
+
+
